7b768f987b5a3aa8e643957dc50b309e861a1bac
Y-Duperray <97739548+Y-Duperray@users.noreply.github.com>
2022-01-22 09:36:29 +0100

    Update and rename HDFSWriter to HDFSWriter.java

--- PrixIndicesDePrix2/src/main/java/Writer/HDFSWriter.java
+++ PrixIndicesDePrix2/src/main/java/Writer/HDFSWriter.java
@@ -6,12 +6,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.execution.datasources.hbase.HBaseTableCatalog;
+import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.spark.sql.Encoders;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.spark.sql.SparkSession;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 public class HDFSWriter implements Consumer<Dataset<Row>> {
