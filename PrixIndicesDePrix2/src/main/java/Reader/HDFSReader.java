97d8b240a6427ed8837f980d054abc338a1e7700
Y-Duperray <97739548+Y-Duperray@users.noreply.github.com>
2022-01-22 09:14:31 +0100

    Update HDFSReader.java

--- PrixIndicesDePrix2/src/main/java/Reader/HDFSReader.java
+++ PrixIndicesDePrix2/src/main/java/Reader/HDFSReader.java
@@ -1,30 +1,49 @@
-package Reader;
+package Readers;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.*;
+import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.spark.sql.SparkSession;
 
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 public class HDFSReader implements Supplier<Dataset<Row>> {
     private FileSystem hdfs;
-    private final String inputPathStr;
+    private String inputPathStr;
+    private SparkSession sparkSession;
 
-    public HDFSReader(String inputFilePath){
-        System.out.println("New FileReader created");
-        this.inputPathStr = inputFilePath;
+    public HDFSReader(String path, SparkSession session){
+        try{
+            this.hdfs = FileSystem.get(new Configuration());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        this.inputPathStr = path;
+        this.sparkSession = session;
     }
 
     @Override
     public Dataset<Row> get() {
-        System.out.println("FileReader.get()");
         try {
-            hdfs = FileSystem.get(new Configuration());
             if(hdfs.exists(new Path(inputPathStr))){
-                FSDataInputStream fsDataInputStream = hdfs.open(new Path(inputPathStr));
+                StringWriter writer = new StringWriter();
+                FSDataInputStream inputStream = hdfs.open(new Path(inputPathStr));
+                IOUtils.copy(inputStream, writer);
+                List<String> lines = Arrays.stream(writer.toString() .split("\n")).filter(l -> !l.startsWith("\"")).collect(Collectors.toList());
+                Dataset<Row> ds = sparkSession.createDataset(lines, Encoders.STRING()).toDF();
+                return ds;
             }
         } catch (Exception e) {
             e.printStackTrace();
